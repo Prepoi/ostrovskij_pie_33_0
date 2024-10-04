35611c14222fb0b875d5e2373f0af7f94c8f2b5d
Prepoi <32ostrov@gmail.com>
2024-10-04 23:29:06 +0700

    Add files via upload

---
+++ lab_2/script.js
@@ -0,0 +1,6 @@
+let a, b;
+a = prompt('Введите значение а: ');
+b = prompt('Введите значение b: ');
+
+let geometricMean = (a*b)**(1/2);
+alert(geometricMean);
---
+++ lab_2/secondScript.js
@@ -0,0 +1,4 @@
+let someNumber;
+someNumber = prompt("Введите трхзначное число:");
+
+alert('Первая цифра числа (число сотен) - ' + Math.floor(someNumber/100))
